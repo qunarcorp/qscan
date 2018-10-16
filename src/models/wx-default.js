09f4a2c835113fbaf40115fc59fca322890da0ac
qingyuan.hou <qingyuan.hou@qunar.com>
2018-10-16 20:50:31 +0800

    fix: wx-wechat

--- src/models/wx-default.js
+++ src/models/wx-default.js
@@ -39,7 +39,7 @@ module.exports = {
         // TODO
         if(!currentVersion || currentVersion.match(/\w=([0-9.]+)/)[1] !== version) {
             logger.warn(`Need version-${version} wechat app`);
-            logger.info(`installing ${version} wxchat...`);
+            logger.info(`installing ${version} wechat...`);
             // install wx 6.7.2
             const apksPath = path.join(__dirname, '../apks'),
                 apkLinePath = 'http://yapkwww.cdn.anzhi.com/data4/apk/201808/20/21134e06c366c63faace92226d3124bb_29305400.apk';
@@ -55,7 +55,7 @@ module.exports = {
                 logger.info('install success');
                 cb(null);
             } else {
-                cb(`Need wxchat-${version}, please install manually`);
+                cb(`Need wechat-${version}, please install manually`);
             }
         } else {
             logger.success('The app version is ok')
