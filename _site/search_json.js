3dcaedf946f1fef99d9e57878443c290ca5d1a5d
wenbo.dong <wenbo.dong@qunar.com>
2018-10-22 16:25:13 +0800

    docs: update

--- _site/search_json.js
+++ _site/search_json.js
@@ -152,9 +152,9 @@ window.ydoc_plugin_search_json = {
       "url": "/experience/index.html",
       "children": [
         {
-          "title": "使用较新的安卓手机",
-          "url": "/experience/index.html#使用较新的安卓手机",
-          "content": "使用较新的安卓手机由于市场上的安卓设备碎片化严重，建议使用 Android 4.4版本以上的、性能较好的安卓手机，性能差的手机打开微信速度较慢，Appium 调用设备的时候可能经常遇到超时问题。"
+          "title": "超时问题",
+          "url": "/experience/index.html#超时问题",
+          "content": "超时问题请使用使用较新的安卓手机：由于市场上的安卓设备碎片化严重，建议使用 Android 4.4版本以上的、性能较好的安卓手机，性能差的手机打开微信速度较慢，Appium 调用设备的时候可能经常遇到超时问题。如果你在使用中遇到问题，请在 github 上面提出 Issue"
         }
       ]
     }
